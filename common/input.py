ede49195ba0df9aae1f446d1582bb0659a2f5090
lingzerol <467264565@qq.com>
2020-02-06 14:03:50 +0800

    initialize

---
+++ common/input.py
@@ -0,0 +1,42 @@
+import platform
+import time
+import sys
+
+if platform.system() == "Windows":
+    import msvcrt
+
+    def input_with_timeout(prompt, timeout, timer=time.monotonic):
+        sys.stdout.write(prompt)
+        sys.stdout.flush()
+        endtime = timer() + timeout
+        result = []
+        while timer() < endtime:
+            if msvcrt.kbhit():
+                result.append(msvcrt.getwche())
+                if result[-1] == '\n':
+                    return ''.join(result[:-1])
+            time.sleep(0.04)
+        print("")
+        return ""
+elif platform.system() == "Linux":
+    import select
+
+    def input_with_timeout(prompt, timeout):
+        sys.stdout.write(prompt)
+        sys.stdout.flush()
+        ready, _, _ = select.select([sys.stdin], [], [], timeout)
+        if ready:
+            return sys.stdin.readline().rstrip('\n')
+        else:
+            print("")
+        return ""
+
+
+def read(in_stream=sys.stdin):
+    result = []
+    c = in_stream.read(1)
+    while c != "\t" and c != " " and c != "\n" or len(result) == 0:
+        if c != "\t" and c != " " and c != "\n":
+            result.append(c)
+        c = in_stream.read(1)
+    return "".join(result)
