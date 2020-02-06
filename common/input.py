f158b5b331307fee959b9f7560e4feccb3852f69
lingzerol <467264565@qq.com>
2020-02-06 16:00:37 +0800

    put platform judgement into the method

--- common/input.py
+++ common/input.py
@@ -4,31 +4,36 @@ import sys
 
 if platform.system() == "Windows":
     import msvcrt
+elif platform.system() == "Linux":
+    import select
 
-    def input_with_timeout(timeout, prompt="", timer=time.monotonic):
-        sys.stdout.write(prompt)
-        sys.stdout.flush()
-        endtime = timer() + timeout
+
+class PlatFormError(RuntimeError):
+    """PlatForm error that present code cannot run in this platform"""
+
+    def __init__(self, args):
+        self.args = args
+
+
+def input_with_timeout(timeout):
+    if platform.system() == "Windows":
+        endtime = time.time() + timeout
         result = []
-        while timer() < endtime:
+        while time.time() < endtime:
             if msvcrt.kbhit():
-                endtime = timer() + timeout
+                endtime = time.time() + timeout
                 result.append(msvcrt.getwche())
                 if result[-1] == '\n' or result[-1] == '\r':
                     print("")
                     return ''.join(result[:-1])
             time.sleep(0.04)
         print("")
-        return ""
-elif platform.system() == "Linux":
-    import select
-
-    def input_with_timeout(timeout, prompt=""):
-        sys.stdout.write(prompt)
-        sys.stdout.flush()
+    elif platform.system() == "Linux":
         ready, _, _ = select.select([sys.stdin], [], [], timeout)
         if ready:
             return sys.stdin.readline().rstrip('\n')
         else:
             print("")
-        return ""
+    else:
+        raise PlatFormError("unsuitable platform.")
+    return ""
